1db75b45f4b3058411a3ec49c735a8d575a4b5c4
faryyym <mohamed.fareem@gmail.com>
2023-11-04 16:44:44 +0500

    Post product, get all products & get product by ID from MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
+
+app.use(express.json())
 
 // routes
 app.get('/', (request, response) => {
@@ -9,6 +12,39 @@ app.get('/', (request, response) => {
 
 app.get('/blog', (request, response) => {
   response.send('Blog Route ')
+})
+
+app.post('/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({
+      message: error.message,
+    })
+  }
+})
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find({})
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    })
+  }
 })
 
 mongoose
