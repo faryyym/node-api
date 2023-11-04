ecd0abe57bb489c0936fcfdd050a02bab02e5b4f
faryyym <mohamed.fareem@gmail.com>
2023-11-04 16:08:10 +0500

    NodeJS Express App connected to MongoDB using Mongoose NPM package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 // routes
@@ -6,6 +7,22 @@ app.get('/', (request, response) => {
   response.send('Hello Node API')
 })
 
-app.listen(3000, () => {
-  console.log('Node API app running')
+app.get('/blog', (request, response) => {
+  response.send('Blog Route ')
 })
+
+mongoose
+  .connect(
+    'mongodb+srv://farym:weh0say@cluster0.xgn3r2l.mongodb.net/ownApi?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('connected to MongoDB')
+
+    // main app init
+    app.listen(3000, () => {
+      console.log('Node API app running')
+    })
+  })
+  .catch((error) => {
+    console.error(error.message)
+  })
