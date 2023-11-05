a7520af5adc18e7e95dbfbbba070cea2f1ddafb4
faryyym <mohamed.fareem@gmail.com>
2023-11-05 22:33:59 +0500

    Ready

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+// IMPORTS
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const productRoute = require('./routes/productRoute')
 const errorMiddleware = require('./middleware/errorMiddleware')
+const cors = require('cors')
 
 const app = express()
 
-const { MONGO_URL, PORT } = process.env
+const { MONGO_URL, PORT, FRONTEND } = process.env
+const corsOptions = {
+  origin: FRONTEND,
+  optionsSuccessStatus: 200,
+}
 
+// USE
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(errorMiddleware)
 
-// routes
+// ROUTES
 app.use('/api/products', productRoute)
 
 app.get('/', (request, response) => {
@@ -22,14 +31,13 @@ app.get('/blog', (request, response) => {
   response.send('Blog Route ')
 })
 
-app.use(errorMiddleware)
-
+// MONGODB SETUP
 mongoose
   .connect(MONGO_URL)
   .then(() => {
     console.log('connected to MongoDB')
 
-    // main app init
+    // APP INIT
     app.listen(PORT, () => {
       console.log(
         'Node API app running on port ' + PORT
